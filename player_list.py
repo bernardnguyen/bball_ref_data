b7909733ca305bb8b34a5bfd657ae73d66eee144
Bernard Nguyen <bernardquynguyen@gmail.com>
2019-09-06 18:12:20 -0700

    fixed bug with adding redundant data and not appending to existing player_list

--- player_list.py
+++ player_list.py
@@ -3,14 +3,18 @@ import pandas as pd
 
 csv_files = glob.glob('data/Season_Totals/*.csv')
 
+years_to_skip = []
 try:
-	player_df = pd.read_csv('data/player_list.csv')
+	players_already_had = pd.read_csv('data/player_list.csv')
+	years_to_skip = list(set(players_already_had['Year']))
 except:
-	player_df = pd.DataFrame(columns=['PlayerID','Name','Year'])
+	pass
+
+player_df = pd.DataFrame(columns=['PlayerID','Name','Year'])
 
 for f in csv_files:	
-	year = f.split('/')[2][:-4]
-	if len(player_df['Year'] == int(f)) != 0:
+	year = int(f.split('/')[2][:-4])
+	if year in years_to_skip:
 		continue
 
 	player_dict = {}
@@ -19,6 +23,7 @@ for f in csv_files:
 	for p in player_list:
 		[player, player_id] = p.split('\\')
 		
+		# Only get player name/ID once per season
 		if player_dict.get(player_id, False):
 			continue
 		else:
@@ -27,7 +32,8 @@ for f in csv_files:
 											'Name':player,
 											'Year':year}, ignore_index=True)
 
+# If expanding, failed to load already exists. Append to that list.
 if os.path.isfile('data/players_failed_to_load.csv'):
 	player_df.to_csv('data/players_failed_to_load.csv',mode='a',index=False,header=False)
-else:
-	player_df.to_csv('data/player_list.csv',mode='a',index=False,header=False)
+# No matter the case, append to the player list file.
+player_df.to_csv('data/player_list.csv',mode='a',index=False,header=False)
